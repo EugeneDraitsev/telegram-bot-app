7f102d84ff7fa63fc6cde1e7f0b2b1ca3117d75a
EugenDraytsev <EugenDraytsev@coherentsolutions.com>
2015-07-21 14:40:55 +0300

    improve image service and google

--- helpers/google.js
+++ helpers/google.js
@@ -1,6 +1,5 @@
 'use strict';
-var http = require('http'),
-    request = require('request');
+var request = require('request');
 
 var google = {
     search: function (query, callback) {
@@ -18,38 +17,6 @@ var google = {
                     callback('Google can\'t find it for you');
                 }
             });
-
-        //// Execute the request
-        //var googleRequest = http.request("http://ajax.googleapis.com/ajax/services/search/images?v=1.0&as_filetype=jpg%20png&rsz=8&q=" + encodeURI(query), function (googleResponse) {
-        //    googleResponse.setEncoding('utf8');
-        //
-        //    // Read the response
-        //    var output = '';
-        //    googleResponse.on('data', function (chunk) {
-        //        output += chunk;
-        //    });
-        //
-        //    // Return the received data when finished, using the specified callback function
-        //    googleResponse.on('end', function () {
-        //        var responseData = JSON.parse(output).responseData;
-        //        if (responseData.results.length > 0) {
-        //            var index = Math.floor(Math.random() * responseData.results.length),
-        //                imageUrl = responseData.results[index].unescapedUrl,
-        //                tbUrl = responseData.results[index].tbUrl;
-        //            google.getImage(imageUrl, callback, tbUrl);
-        //        }
-        //        else {
-        //            callback('Google can\'t find it for you');
-        //        }
-        //    });
-        //});
-        //
-        //// Log errors and return an error message instead of weather message
-        //googleRequest.on('error', function () {
-        //    callback('Sorry, could not get the data from Google.');
-        //});
-        //
-        //googleRequest.end();
     },
 
     getImage: function (url, callback, tbUrl) {
