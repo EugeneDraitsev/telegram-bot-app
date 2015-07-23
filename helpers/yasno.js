a9fe6cebe2e7b5ecdbff5fdf0ce98e8bd12739a6
EugenDraytsev <EugenDraytsev@coherentsolutions.com>
2015-07-23 11:35:09 +0300

    yasno refactoring

--- helpers/yasno.js
+++ helpers/yasno.js
@@ -2,16 +2,15 @@
 
 var yasno = {
     yasnyfy: function (text) {
+        var year = new Date().getYear() + 1900;
+        var yasnenko = "\n>Ясно";
         if (text.length === 0) {
-            return text;
+            text = ("\n>" + year + yasnenko);
         }
         else {
-            var year = new Date().getYear() + 1900;
-            var yasnenko = "\n>Ясно";
-            text = ("\n>" + year +"\n>" + text +yasnenko);
-            return text;
+            text = ("\n>" + year + "\n>" + text + yasnenko);
         }
-
+        return text;
     }
 };
 module.exports = yasno;
