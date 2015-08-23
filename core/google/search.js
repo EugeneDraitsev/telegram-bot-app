f3c421c110e4c026c7370efeaae9712fe3db8cb8
Eugene Draitsev <ddrra@gmail.com>
2015-08-23 15:33:31 +0300

    replace method getImage from google/search to image/png

--- core/google/search.js
+++ core/google/search.js
@@ -1,6 +1,7 @@
 'use strict';
 var request = require('request'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    imageService = request('../image/png');
 
 var google = {
     searchImage: function (query, callback) {
@@ -12,36 +13,13 @@ var google = {
                     var image = _.sample(responseData.results),
                         imageUrl = image.unescapedUrl,
                         tbUrl = image.tbUrl;
-                    google.getImage(imageUrl, callback, tbUrl);
+                    imageService.getImage(imageUrl, callback, tbUrl);
                 }
                 else {
                     callback('Google can\'t find it for you');
                 }
             }).on('error', function (e) {
                 console.log('ERROR getting search result from google:' + e);
-            });
-    },
-
-    getImage: function (url, callback, tbUrl) {
-        request
-            .get(url)
-            .on('response', function (response) {
-                if (!response.headers || !response.headers['content-type']) {
-                    callback('can\'t load image');
-                    return;
-                }
-                if (response.headers['content-type'].split('/')[0] !== 'image') {
-                    if (!tbUrl) {
-                        callback('can\'t load even preview');
-                        return;
-                    }
-                    google.getImage(tbUrl, callback);
-                    return;
-                }
-                callback(false, response, url);
-            }).on('error', function (e) {
-                console.log('ERROR uploading pic from server:' + e);
-                google.getImage(tbUrl, callback);
             });
     }
 };
