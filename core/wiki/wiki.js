202c17a2e62cf27a18a453950774a6f64ac31475
Yev Kovalev <yauheni.kavaliou@sephora.com>
2016-08-15 13:54:19 +0300

    Applied another batch of notes

--- core/wiki/wiki.js
+++ core/wiki/wiki.js
@@ -1,31 +1,29 @@
 "use strict";
-var request = require('request'),
-    Q = require('q'),
-    _ = require('lodash');
+var request = require('request');
+
+var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=1&namespace=0&search=";
 
 var wiki = {
     search: function(searchTerm) {
-        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=1&namespace=0&search=" +
-                searchTerm;
-        var deferred = Q.defer();
+        return new Promise(function (resolve, reject) {
+            var target = url + searchTerm;
+            request.get(target, function (err, response, body) {
+                if (err || !body) {
+                    console.log(err);
+                    reject(err);
+                }
 
-        request.get(url, function (err, response, body) {
-            if (err || !body) {
-                console.log(err);
-                return deferred.resolve(err);
-            }
-
-            var response = JSON.parse(body);
-            var link = response[3].length === 0 ?
-                    'Failed to find article for term: ' + response[0] :
-                    response[3];
-            deferred.resolve(link);
-        }).on('error', function (e) {
-            console.log('ERR Failed to fetch date from wiki opensearch:' + e);
-            deferred.resolve('ERR Failed to fetch date from wiki opensearch');
+                var response = JSON.parse(body);
+                var link = response[3];
+                if (link.length === 0) {
+                    link = 'Failed to find article for term: ' + response[0];
+                }
+                resolve(link);
+            }).on('error', function (e) {
+                console.log('ERR Failed to fetch date from wiki opensearch:' + e);
+                reject(e);
+            });
         });
-
-        return deferred.promise();
     }
 }
 
