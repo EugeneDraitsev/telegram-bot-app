cded5bc8e930c674a403e506645341b23fd8fee2
Eugene Draitsev <ddrrai@gmail.com>
2016-08-16 10:06:34 +0300

    added try-catch wrapper for wiki search

--- core/wiki/wiki.js
+++ core/wiki/wiki.js
@@ -4,27 +4,32 @@ var request = require('request');
 var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=1&namespace=0&search=";
 
 var wiki = {
-    search: function(searchTerm) {
-        return new Promise(function (resolve, reject) {
-            var target = url + searchTerm;
-            request.get(target, function (err, response, body) {
-                if (err || !body) {
-                    console.log(err);
-                    reject(err);
-                }
+  search: function (searchTerm) {
+    return new Promise(function (resolve, reject) {
+      try {
+        var target = url + searchTerm;
+        request.get(target, function (err, response, body) {
+          if (err || !body) {
+            console.log(err);
+            reject(err);
+          }
 
-                var response = JSON.parse(body);
-                var link = response[3];
-                if (!link || link.length === 0) {
-                    link = 'Failed to find article for term: ' + searchTerm;
-                }
-                resolve(link);
-            }).on('error', function (e) {
-                console.log('ERR Failed to fetch date from wiki opensearch:' + e);
-                reject(e);
-            });
+          var response = JSON.parse(body);
+          var link = response[3];
+          if (!link || link.length === 0) {
+            link = 'Failed to find article for term: ' + searchTerm;
+          }
+          resolve(link);
+
+        }).on('error', function (e) {
+          console.log('ERR Failed to fetch date from wiki opensearch:' + e);
+          reject(e);
         });
-    }
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
 };
 
 module.exports = wiki;
