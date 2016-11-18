698798adfe1ae528b409b3a68e887d7f59f17895
EugeneDraitsev <ddrrai@gmail.com>
2016-11-18 23:35:52 +0300

    Initial lambdas config

--- core/image/png.js
+++ core/image/png.js
@@ -1,6 +1,5 @@
 'use strict';
-var PNGImage = require('pngjs-image'),
-    Q = require('q'),
+var Q = require('q'),
     request = require('request');
 
 var imageService = {
@@ -47,27 +46,6 @@ var imageService = {
         function reject() {
             deferred.reject('can\'t load image\n' + url);
         }
-
-        return deferred.promise;
-    },
-
-    getTestImage: function () {
-        var image = PNGImage.createImage(400, 400),
-            deferred = Q.defer();
-        for (var i = 0; i < 400; i++) {
-            for (var j = 0; j < 400; j++) {
-                image.setAt(i, j, {
-                    red: 255 * Math.random(),
-                    green: 255 * Math.random(),
-                    blue: 255 * Math.random(),
-                    alpha: 255
-                });
-            }
-        }
-
-        image.toBlob(function (err, image) {
-            deferred.resolve(image);
-        });
 
         return deferred.promise;
     }
--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 'use strict';
-var express = require('express'),
-    telegram = require('../core/telegram/telegram.js'),
-    google = require('../core/google/search.js'),
-    huiator = require('../core/text/huiator.js'),
-    yasno = require('../core/text/yasno.js'),
-    magic8ball = require('../core/stickers/magic8ball'),
-    translation = require('../core/yandex/translation.js'),
-    currency = require('../core/currency/currency.js'),
-    statistic = require('../core/statistic/statistic'),
-    ChatStatistic = require('../core/models/chat-statistic'),
-    youtube = require('../core/google/youtube'),
-    wiki = require('../core/wiki/wiki'),
-    _ = require('lodash'),
-    router = express.Router();
+try {
+    var config = require('./config')
+    for (var key in config) {
+        process.env[key] = config[key]
+    }
+} catch (err) {
+    // ignore
+}
 
-router.post('/', function (req, res) {
-    if (!req.body || !req.body.message || !req.body.message.chat || !req.body.message.message_id || !req.body.message.text) {
-        res.statusCode = 200;
-        res.end(null);
-        return;
+var telegram = require('./core/telegram/telegram.js'),
+    google = require('./core/google/search.js'),
+    huiator = require('./core/text/huiator.js'),
+    yasno = require('./core/text/yasno.js'),
+    magic8ball = require('./core/stickers/magic8ball'),
+    translation = require('./core/yandex/translation.js'),
+    currency = require('./core/currency/currency.js'),
+    statistic = require('./core/statistic/statistic'),
+    ChatStatistic = require('./core/models/chat-statistic'),
+    youtube = require('./core/google/youtube'),
+    wiki = require('./core/wiki/wiki'),
+    _ = require('lodash');
+
+exports.handler = (req, context, callback) => {
+    if (!req || !req.message || !req.message.chat || !req.message.message_id || !req.message.text) {
+        return context.succeed("Hum. who are you?");
     }
 
-    var telegramUpdate = req.body,
+    var telegramUpdate = req,
         telegramMessage = telegramUpdate.message.text,
         chat_id = telegramUpdate.message.chat.id,
         reply_to_message_id = telegramUpdate.message.message_id,
         user_info = telegramUpdate.message.from;
 
-    statistic.updateStatistic(telegramMessage, user_info, chat_id);
+    //statistic.updateStatistic(telegramMessage, user_info, chat_id);
 
     if (telegramMessage.lastIndexOf('/g', 0) === 0) {
         var query = parseQuery(telegramMessage);
@@ -87,34 +92,34 @@ router.post('/', function (req, res) {
         telegram.sendMessage(chat_id, statistic.getUsersDayStatistic(chat_id), '');
     }
 
-    if (telegramMessage.lastIndexOf('/z') === 0) {
-        var chatStatistic = statistic.getChatStatistic(chat_id), message = 'User Statistic.';
-
-        ChatStatistic.update({chat_id: chat_id}, {
-            chat_id: chat_id,
-            users: chatStatistic.users
-        }, {upsert: true}, function (err) {
-            if (err) {
-                console.log('stat update error: ' + err);
-            }
-        });
-
-        chatStatistic.users.sort(function (a, b) {
-            return b.msgCount - a.msgCount;
-        });
-
-        var messagesCount = chatStatistic.users.reduce(function (a, b) {
-            return a + b.msgCount;
-        }, 0);
-
-        message += '\nAll messages: ' + messagesCount;
-
-        chatStatistic.users.forEach(function (a) {
-            message += '\n' + a.msgCount + ' (' + (a.msgCount / messagesCount * 100).toFixed(2) + '%) - ' + a.username;
-        }, 0);
-
-        telegram.sendMessage(chat_id, message, '');
-    }
+    // if (telegramMessage.lastIndexOf('/z') === 0) {
+    //     var chatStatistic = statistic.getChatStatistic(chat_id), message = 'User Statistic.';
+    //
+    //     ChatStatistic.update({chat_id: chat_id}, {
+    //         chat_id: chat_id,
+    //         users: chatStatistic.users
+    //     }, {upsert: true}, function (err) {
+    //         if (err) {
+    //             console.log('stat update error: ' + err);
+    //         }
+    //     });
+    //
+    //     chatStatistic.users.sort(function (a, b) {
+    //         return b.msgCount - a.msgCount;
+    //     });
+    //
+    //     var messagesCount = chatStatistic.users.reduce(function (a, b) {
+    //         return a + b.msgCount;
+    //     }, 0);
+    //
+    //     message += '\nAll messages: ' + messagesCount;
+    //
+    //     chatStatistic.users.forEach(function (a) {
+    //         message += '\n' + a.msgCount + ' (' + (a.msgCount / messagesCount * 100).toFixed(2) + '%) - ' + a.username;
+    //     }, 0);
+    //
+    //     telegram.sendMessage(chat_id, message, '');
+    // }
 
     if (telegramMessage.lastIndexOf('/8', 0) === 0) {
         telegram.sendSticker(chat_id, magic8ball.prediction(), reply_to_message_id);
@@ -143,13 +148,8 @@ router.post('/', function (req, res) {
                 console.log("Search couldn't be completed: " + err);
             });
     }
-
-    res.statusCode = 200;
-    res.end(null);
-});
+};
 
 function parseQuery(query) {
     return query.replace(/\/\S+\s*/g, '').trim();
 }
-
-module.exports = router;
