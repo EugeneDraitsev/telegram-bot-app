dfdcd8ea2fe7bfc0b42139b68509fb5ba5f38e27
EugeneDraitsev <ddrrai@gmail.com>
2016-11-27 08:18:28 +0300

    fixed youtube videos encodeUrl bug. Some changes in mock data for testing

--- core/google/youtube.js
+++ core/google/youtube.js
@@ -7,7 +7,7 @@ const BASE_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type
 const RESULT_PREFIX = 'https://youtu.be/'
 
 function search(query) {
-  return rp(`${BASE_URL}&key=${YOUTUBE_TOKEN}&q=${query}&maxResults=${8}`)
+  return rp(`${BASE_URL}&key=${YOUTUBE_TOKEN}&q=${encodeURI(query)}&maxResults=${8}`)
     .then(response => `${RESULT_PREFIX}${_.sample(JSON.parse(response).items.map(item => item.id.videoId))}`)
     .catch((e) => {
       console.log(e)
