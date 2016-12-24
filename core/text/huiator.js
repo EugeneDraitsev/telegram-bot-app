e70f55c5df06d6252117b751f894e6f9f9f6d178
EugeneDraitsev <ddrrai@gmail.com>
2016-12-24 17:25:12 +0300

    fixed huify (again)

--- core/text/huiator.js
+++ core/text/huiator.js
@@ -1,13 +1,12 @@
 'use strict'
 
 const _ = require('lodash')
-
 const huify = (text) => text.replace(/[А-Я0-9]+/ig, huifyWord)
-
 const consonants = ['бвгджзйклмнпрстфхчцшщ']
-const patterns = [new RegExp(`^[${consonants}]*[оеёэ]`), new RegExp(`^[${consonants}]*[ую]`),
-  new RegExp(`^[${consonants}]*[ая]`), new RegExp(`^[${consonants}]*[иы]`)]
-const mainPattern = new RegExp(`^[${consonants}].`)
+
+const patterns = [new RegExp(`^[${consonants}]*[оеёэ]`, 'i'), new RegExp(`^[${consonants}]*[ую]`, 'i'),
+  new RegExp(`^[${consonants}]*[ая]`, 'i'), new RegExp(`^[${consonants}]*[иы]`, 'i')]
+const mainPattern = new RegExp(`^[${consonants}]*.`, 'i')
 
 function huifyWord(word) {
   switch (_.findIndex(patterns, pattern => pattern.test(word))) {
