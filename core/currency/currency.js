23ff1567dcbd20ff5188fc7e4a47f25ce7fdd54d
edraitsev <Eugene.Draitsev@ebuilder.com>
2017-06-15 20:25:09 +0300

    Added cryptocompare integration

--- core/currency/currency.js
+++ core/currency/currency.js
@@ -3,8 +3,12 @@ const parseString = require('xml2js').parseString
 const _ = require('lodash')
 
 function getCurrency() {
-  return Promise.all([getBelarusCurrency().catch(() => 'error getting currency from nbrb'),
-    getRussianCurrency().catch(() => 'error getting currency from meduza')])
+  const promises = [
+    getBelarusCurrency().catch(() => 'error getting currency from nbrb'),
+    getRussianCurrency().catch(() => 'error getting currency from meduza'),
+    getCryptoCurrency().catch(() => 'error getting currency from cryptocompare')
+  ]
+  return Promise.all(promises)
     .then((result) => {
       return `Курсы валют:\n\n${result.join('\n')}`
     })
@@ -16,9 +20,9 @@ function getRussianCurrency() {
   return rp.get({url, timeout: 10000}).then((response) => {
     const currency = JSON.parse(response)
     return 'Курсы медузы:\n' + Object.keys(currency)
-        .filter(currency => _.includes(currencyCodes, currency))
-        .reduce((message, key) =>
-          message.concat(`${key.toUpperCase()}: ${Number(currency[key].current).toFixed(2)}\n`), '')
+      .filter(currency => _.includes(currencyCodes, currency))
+      .reduce((message, key) =>
+        message.concat(`${key.toUpperCase()}: ${Number(currency[key].current).toFixed(2)}\n`), '')
   })
 }
 
@@ -29,12 +33,23 @@ function getBelarusCurrency() {
     return new Promise(resolve => {
       parseString(result, (err, result) => {
         const message = 'Курсы НБРБ:\n' + result.DailyExRates.Currency
-            .filter(currency => _.includes(currencyCodes, currency.CharCode[0]))
-            .reduce((message, currency) =>
-              message.concat(`${currency.CharCode[0]}: ${Number(currency.Rate).toFixed(4)}\n`), '')
+          .filter(currency => _.includes(currencyCodes, currency.CharCode[0]))
+          .reduce((message, currency) =>
+            message.concat(`${currency.CharCode[0]}: ${Number(currency.Rate).toFixed(4)}\n`), '')
         resolve(message)
       })
     })
+  })
+}
+
+function getCryptoCurrency() {
+  const url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP&tsyms=USD'
+  return rp.get({url, timeout: 10000}).then((response) => {
+    const currency = JSON.parse(response)
+    return 'Курсы криптовалют:\n' +
+      Object.keys(currency)
+        .reduce((message, key) =>
+          message.concat(`${key}: ${currency[key].USD}\n`), '')
   })
 }
 
