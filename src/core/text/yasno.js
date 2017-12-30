b3e54907bc2256f076fcd0ea2e2030d5679eb6a6
edraitsev <ddrrai@gmail.com>
2017-12-30 00:58:05 +0000

    minor yasnyfy improvement

--- src/core/text/yasno.js
+++ src/core/text/yasno.js
@@ -1,5 +1,10 @@
+const YEARS = {
+  2017: '2k17',
+  2018: '20!8',
+}
+
 const yasnyfy = (text) => {
-  const year = String(new Date().getFullYear()).replace('0', 'k')
+  const year = YEARS[new Date().getFullYear()]
   return `\n>${year}${text ? `\n>${text}` : ''}\nЯсно`
 }
 
