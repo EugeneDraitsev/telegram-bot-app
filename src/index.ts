c0205f0d7f95d9daaef764aa28c06bd5dcd028c9
edraitsev <Eugene.Draitsev@ebuilder.com>
2018-05-14 11:17:35 +0200

    Deploy simplified

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import '../load-config.js'
 
-import { Callback, Context, Handler } from 'aws-lambda'
+import { Handler } from 'aws-lambda'
 import { get, noop } from 'lodash'
 import { processQuery } from './commands/'
 import { closeConnection, openConnection, updateStatistic } from './core'
-
-interface IResponse {
-  statusCode: number
-  body: string
-}
 
 function updateMessageStat(user_info: any, chat_id: any) {
   return openConnection().then(() => updateStatistic(user_info, chat_id))
@@ -22,25 +17,28 @@ function processRequest(req: any) {
   const { message: { message_id, from, chat, text, reply_to_message } } = req
   const replyText = get(reply_to_message, 'text')
 
-  return Promise.all([processQuery(text, message_id, chat.id, replyText).catch(noop),
-    updateMessageStat(from, chat.id).catch(noop)])
+  return Promise.all([
+    processQuery(text, message_id, chat.id, replyText).catch(noop),
+    updateMessageStat(from, chat.id).catch(noop),
+  ])
     .then(closeConnection)
 }
 
-function sendResponse(message: any, input: string, callback: (obj: any, response: IResponse) => void) {
-  const response = {
-    body: JSON.stringify({ message, input }),
-    statusCode: 200,
-  }
-
-  return callback(null, response)
-}
-
-export const handler: Handler = (event: any, context: Context, callback: Callback) => {
+export const handler: Handler = async (event: any) => {
   const body = event.body ? JSON.parse(event.body) : event
   console.log(body) // tslint:disable-line:no-console
 
-  return processRequest(body)
-    .then(message => sendResponse(message, body, callback))
-    .catch(() => sendResponse('something going wrong :c', body, callback))
+  try {
+    const message = await processRequest(body)
+    return {
+      body: JSON.stringify({ body, message }),
+      statusCode: 200,
+    }
+
+  } catch (e) {
+    return {
+      body: JSON.stringify({ body, message: 'something going wrong :c' }),
+      statusCode: 200,
+    }
+  }
 }
