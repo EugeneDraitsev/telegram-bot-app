2568f012c5e4286621158e030da15daf5df3e5d8
edraitsev <Eugene.Draitsev@ebuilder.com>
2018-05-14 12:33:04 +0200

    Moved horoscope to rambler

--- src/core/horoscope.ts
+++ src/core/horoscope.ts
@@ -1,5 +1,4 @@
 import fetch from 'node-fetch'
-import { parseString } from 'xml2js'
 
 const rus = [
   'овен',
@@ -35,24 +34,15 @@ function getAstrologicalSigns(query: string) {
   return eng[rus.indexOf(query.toLowerCase())]
 }
 
-interface IHoroscope {
-  horo: {} []
-}
-
 export const getHoroscope = async (query: string) => {
   const sign = getAstrologicalSigns(query)
-  const url = 'http://img.ignio.com/r/export/utf/xml/daily/com.xml'
+  const url = `https://horoscopes.rambler.ru/api/front/v1/horoscope/today/${sign}/`
 
   if (!sign) {
     return 'Нужен Ваш зодиакальный знак'
   }
 
-  const response = await fetch(url, { timeout: 10000 })
-  const xml = await response.text()
+  const { text } = await fetch(url, { timeout: 10000 }).then(x => x.json())
 
-  const result = await new Promise((resolve, reject) =>
-    parseString(xml, (err, res: IHoroscope) => err ? reject(err) : resolve(res))) as IHoroscope
-
-  const prediction = result.horo[sign][0]
-  return `${query}\nВчера:${prediction.yesterday}\nСегодня:${prediction.today}\nЗавтра:${prediction.tomorrow}`
+  return text
 }
