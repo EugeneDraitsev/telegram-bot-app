8ac127f8178d669e4ef430f84642552163f6d598
edraitsev <Eugene.Draitsev@ebuilder.com>
2018-06-02 13:59:17 +0200

    xray temporary disabled

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Handler } from 'aws-lambda'
 import { get, noop } from 'lodash'
 
 // tslint:disable-next-line
-const AWSXRay = require('aws-xray-sdk')
+// const AWSXRay = require('aws-xray-sdk')
 
 import { processQuery } from './commands/'
 import { closeConnection, openConnection, updateStatistic } from './core'
@@ -13,32 +13,51 @@ function updateMessageStat(user_info: any, chat_id: any) {
   return openConnection().then(() => updateStatistic(user_info, chat_id))
 }
 
+// function processRequest(req: any) {
+//   return new Promise((resolve, reject) => {
+//     const segment = new AWSXRay.Segment('telegram-bot')
+//     const namespace = AWSXRay.getNamespace()
+//     namespace.run(() => {
+//       AWSXRay.setSegment(segment)
+//       AWSXRay.captureAsyncFunc('processRequest', (subsegment: any) => {
+//         if (!req || !req.message || !req.message.chat || !req.message.text) {
+//           return Promise.resolve('not a telegram message')
+//         }
+//
+//         const { message: { message_id, from, chat, text, reply_to_message } } = req
+//         const replyText = get(reply_to_message, 'text')
+//
+//         return Promise.all([
+//           processQuery(text, message_id, chat.id, replyText).catch(noop),
+//           updateMessageStat(from, chat.id).catch(noop),
+//         ])
+//           .then(closeConnection)
+//           .then((res: any) => resolve(res))
+//           .catch(reject)
+//           .finally(() => {
+//             subsegment.close()
+//             segment.close()
+//           })
+//       })
+//     })
+//   })
+// }
+
 function processRequest(req: any) {
-  return new Promise((resolve, reject) => {
-    const segment = new AWSXRay.Segment('telegram-bot')
-    const namespace = AWSXRay.getNamespace()
-    namespace.run(() => {
-      AWSXRay.setSegment(segment)
-      AWSXRay.captureAsyncFunc('processRequest', (subsegment: any) => {
-        if (!req || !req.message || !req.message.chat || !req.message.text) {
-          return Promise.resolve('not a telegram message')
-        }
+  if (!req || !req.message || !req.message.chat || !req.message.text) {
+    return Promise.resolve('not a telegram message')
+  }
 
-        const { message: { message_id, from, chat, text, reply_to_message } } = req
-        const replyText = get(reply_to_message, 'text')
+  const { message: { message_id, from, chat, text, reply_to_message } } = req
+  const replyText = get(reply_to_message, 'text')
 
-        return Promise.all([
-          processQuery(text, message_id, chat.id, replyText).catch(noop),
-          updateMessageStat(from, chat.id).catch(noop),
-        ])
-          .then(closeConnection)
-          .then((res: any) => resolve(res))
-          .catch(reject)
-          .finally(() => subsegment.close())
-      })
-    })
-  })
+  return Promise.all([
+    processQuery(text, message_id, chat.id, replyText).catch(noop),
+    updateMessageStat(from, chat.id).catch(noop),
+  ])
+    .then(closeConnection)
 }
+
 
 export const handler: Handler = async (event: any) => {
   const body = event.body ? JSON.parse(event.body) : event
