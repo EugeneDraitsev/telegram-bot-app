1114d10a9981c80594d8c8b726bf2bce7add2024
edraitsev <ddrrai@gmail.com>
2018-09-09 19:52:30 +0200

    Now we store user info instead of name and id

--- src/core/aws/dynamo/chat-events.ts
+++ src/core/aws/dynamo/chat-events.ts
@@ -1,15 +1,14 @@
 import { random } from 'lodash'
 
 import { dynamoPutItem } from '../../../utils'
-import { getUserName, IUserInfo } from './'
+import { IUserInfo } from './'
 
 export const saveEvent = async (userInfo: IUserInfo, chat_id: string, date: number, XRaySegment: any) => {
   const event = {
+    userInfo,
     // trying to avoid lost messages
     date: date * 1000 + random(-500, 500),
     chatId: String(chat_id),
-    userId: String(userInfo.id),
-    name: getUserName(userInfo),
   }
 
   const params = {
