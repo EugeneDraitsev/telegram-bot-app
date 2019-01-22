e9372aada5df861b0e8e9e21545ebccbc94d306f
Eugene Draitsev <Eugene.Draitsev@ebuilder.com>
2019-01-22 14:09:42 +0100

    tests fix

--- src/core/text/__tests__/shrug.test.ts
+++ src/core/text/__tests__/shrug.test.ts
@@ -3,6 +3,6 @@ import { shrugyfy } from '../'
 describe('shrugyfy should works as designed', () => {
   test('shrugyfy should properly work without any values', () => {
 
-    expect(shrugyfy()).toEqual(`¯\_(ツ)_/¯`)
+    expect(shrugyfy()).toEqual('\`¯\\_(ツ)_/¯\`')
   })
 })
