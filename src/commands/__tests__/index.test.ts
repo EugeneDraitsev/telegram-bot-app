5b8534a4ef1da8eb2c3c45619ac0a73290d2949b
edraitsev <ddrrai@gmail.com>
2019-01-23 15:49:46 +0100

    added tests for parseQuery

--- src/commands/__tests__/index.test.ts
+++ src/commands/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { findCommand } from '../'
+import { findCommand, parseQuery } from '../'
 
 describe('findCommand must works as designed', () => {
   test('findCommand must properly commands from first word in message or string ending with @', () => {
@@ -6,5 +6,19 @@ describe('findCommand must works as designed', () => {
     expect(findCommand('/hello world')).toBeUndefined()
     expect(findCommand('/g@draiBot')).toEqual('/g')
     expect(findCommand('g')).toBeUndefined()
+  })
+})
+
+describe('parseQuery should works as designed', () => {
+  test('parseQuery should correct handle empty commands', () => {
+    expect(parseQuery('/g')).toEqual('')
+    expect(parseQuery('')).toEqual('')
+    expect(parseQuery('/g@draiBot')).toEqual('')
+  })
+  test('parseQuery should properly parse different types of commands', () => {
+    expect(parseQuery('/hello world')).toEqual('world')
+    expect(parseQuery('/g cats')).toEqual('cats')
+    expect(parseQuery('/g@draiBot cat')).toEqual('cat')
+    expect(parseQuery('/g@draiBot testing is cool')).toEqual('testing is cool')
   })
 })
