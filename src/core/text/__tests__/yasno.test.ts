1e13d96d0e567ea6636f9f60067226d6b54a3dd3
pawa allo <pawa.allo@yandex.ru>
2019-04-01 11:45:05 +0000

    Merged in foolday (pull request #53)
    haha
    
    * haha
    
    * Added 1st April identification
    
    * test fixes
    
    Approved-by: Eugene Draitsev <ddrrai@gmail.com>

--- src/core/text/__tests__/yasno.test.ts
+++ src/core/text/__tests__/yasno.test.ts
@@ -1,13 +1,21 @@
+import { DateTime } from 'luxon'
+
 import { yasnyfy } from '../'
 
 describe('yasnyfy should works as designed', () => {
+  const { month, day } = DateTime.local().setZone('Europe/Minsk').toObject()
+
   test('yasnyfy should properly work with hardcoded values', () => {
-    expect(yasnyfy('тест', '2018')).toEqual('\n>20!8\n>тест\nЯсно')
-    expect(yasnyfy('', '2018')).toEqual('\n>20!8\nЯсно')
+    if (month !== 4 || day !== 1) {
+      expect(yasnyfy('тест', '2018')).toEqual('\n>20!8\n>тест\nЯсно')
+      expect(yasnyfy('', '2018')).toEqual('\n>20!8\nЯсно')
+    }
   })
   test('yasnyfy should properly work with ordinary values', () => {
-    expect(yasnyfy('тест', '2019')).toEqual('\n>2k19\n>тест\nЯсно')
-    expect(yasnyfy('тест', '3019')).toEqual('\n>3k19\n>тест\nЯсно')
-    expect(yasnyfy('', '3019')).toEqual('\n>3k19\nЯсно')
+    if (month !== 4 || day !== 1) {
+      expect(yasnyfy('тест', '2019')).toEqual('\n>2k19\n>тест\nЯсно')
+      expect(yasnyfy('тест', '3019')).toEqual('\n>3k19\n>тест\nЯсно')
+      expect(yasnyfy('', '3019')).toEqual('\n>3k19\nЯсно')
+    }
   })
 })
--- src/core/text/yasno.ts
+++ src/core/text/yasno.ts
@@ -1,9 +1,16 @@
+import { DateTime } from 'luxon'
+
 const YEARS = {
   2017: '2k17',
   2018: '20!8',
 }
 
 export const yasnyfy = (text: string, year: string) => {
+  const { month, day } = DateTime.local().setZone('Europe/Minsk').toObject()
   const formattedYear = YEARS[year] || year.replace('0', 'k')
+
+  if (month === 4 && day === 1) {
+    return `\n>1 Апреля ${year.slice(2)} года${text ? `\n>${text}` : ''}\nЯсно😐`
+  }
   return `\n>${formattedYear}${text ? `\n>${text}` : ''}\nЯсно`
 }
