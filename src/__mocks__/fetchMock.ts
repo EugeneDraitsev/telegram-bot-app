bdcbcf2edfad125a0e73b04fa2b8efcec4797f74
Eugene Draitsev <ddrrai@gmail.com>
2019-11-10 12:19:25 +0100

    Fixed minor lint issue

--- src/__mocks__/fetchMock.ts
+++ src/__mocks__/fetchMock.ts
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch'
 import { FetchMock } from 'jest-fetch-mock'
 
-// eslint-disable-next-line eol-last
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const fetchMock: FetchMock = fetch as any
