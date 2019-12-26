c94b3d973bca65d6e40233c2fd58833925b86799
crw614 <kapernik.b@gmail.com>
2019-12-26 23:34:32 +0300

    Move values in constants

--- src/core/text/yasno.ts
+++ src/core/text/yasno.ts
@@ -7,14 +7,15 @@ const YEARS = {
 }
 
 const whDate = (): string => { // https://warhammer40k.fandom.com/wiki/Imperial_Dating_System
+  const secondsInYear = 31556926
+  const secondsInFraction = 31557
   const currentTimeInSeconds = Math.floor(DateTime.local().setZone('Europe/Minsk').toMillis() / 1000)
-  const yearsFromEpochStart = Math.floor(currentTimeInSeconds / 31556926)
-  const currentYearStart = yearsFromEpochStart * 31556926
-  const yearFraction = Math.floor((currentTimeInSeconds - currentYearStart) / 31557)
+  const yearsFromEpochStart = Math.floor(currentTimeInSeconds / secondsInYear)
+  const currentYearStart = yearsFromEpochStart * secondsInYear
+  const yearFraction = Math.floor((currentTimeInSeconds - currentYearStart) / secondsInFraction)
   const currentYear = yearsFromEpochStart + 1970
   const millenium = Math.floor(currentYear / 1000) + 1
   return `0 ${yearFraction} ${String(currentYear).slice(1, 4)}.M${millenium}`
-  // 0 123 456.M41 = Check Number/Year Fraction/Year/Millennium
 }
 export const yasnyfy = (text: string): string => {
   const { month, day, year } = DateTime.local().setZone('Europe/Minsk').toObject()
