e9609dfddccc3386132afa6920210e3287a9bfa5
crw614 <kapernik.b@gmail.com>
2019-12-26 23:17:01 +0300

    implemented Imperial Dating System for yasno

--- src/core/text/__tests__/yasno.test.ts
+++ src/core/text/__tests__/yasno.test.ts
@@ -12,11 +12,13 @@ describe('yasnyfy should works as designed', () => {
 
   test('yasnyfy should properly work with ordinary values', () => {
     Settings.now = (): number => new Date(2019, 10, 10).valueOf()
-
     expect(yasnyfy('тест')).toEqual('\n>2k19\n>тест\nЯсно')
-    Settings.now = (): number => new Date(3019, 10, 10).valueOf()
-    expect(yasnyfy('тест')).toEqual('\n>3k19\n>тест\nЯсно')
-    expect(yasnyfy('')).toEqual('\n>3k19\nЯсно')
+    expect(yasnyfy('')).toEqual('\n>2k19\nЯсно')
+  })
+  test('yasnyfy should properly work with warhammer values', () => {
+    Settings.now = (): number => new Date(2021, 10, 10).valueOf()
+    expect(yasnyfy('тест').slice(7)).toEqual(' 021.M3\n>тест\nЯсно')
+    expect(yasnyfy('').slice(7)).toEqual(' 021.M3\nЯсно')
   })
 
   test('yasnyfy should properly work at 1st of april', () => {
