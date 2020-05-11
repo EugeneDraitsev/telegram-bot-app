ffe3928ac079b227e2d5c2d5876496396a433499
Eugene Draitsev <ddrrai@gmail.com>
2020-05-11 21:02:23 +0200

    Updated dependencies

--- src/core/text/__tests__/dice.test.ts
+++ src/core/text/__tests__/dice.test.ts
@@ -1,10 +1,12 @@
+import lodash from 'lodash'
+
 import { throwDice } from '..'
 
-const lodash = require.requireActual('lodash')
+lodash.random = jest.fn(() => 1)
 
 describe('throwDice should works as designed', () => {
   test('throwDice should return correct dices', () => {
-    lodash.random = jest.fn(() => 1)
+    // lodash.random = jest.fn(() => 1)
     expect(throwDice(6)).toEqual(`\`\`\`
   ___
 /     \\
