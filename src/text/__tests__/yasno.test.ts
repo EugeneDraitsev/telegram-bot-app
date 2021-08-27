9b67a00b129dc7e218d6bcf70468160b8f9c21d5
Eugene Draitsev <ddrrai@gmail.com>
2021-08-27 17:41:35 +0200

    Remove luxon from dependencies

--- src/text/__tests__/yasno.test.ts
+++ src/text/__tests__/yasno.test.ts
@@ -1,43 +1,44 @@
-import { Settings } from 'luxon'
-
 import { yasnyfy } from '..'
 
 describe('yasnyfy should works as designed', () => {
   test('yasnyfy should properly work with hardcoded values', () => {
-    Settings.now = (): number => new Date(2018, 10, 10).valueOf()
+    jest.useFakeTimers('modern')
+    jest.setSystemTime(new Date(2018, 10, 10))
 
     expect(yasnyfy('тест')).toEqual('\n>20!8\n>тест\nЯсно')
     expect(yasnyfy('')).toEqual('\n>20!8\nЯсно')
   })
 
   test('yasnyfy should properly work with ordinary values', () => {
-    Settings.now = (): number => new Date(2017, 10, 10).valueOf()
+    jest.useFakeTimers('modern')
+    jest.setSystemTime(new Date(2017, 10, 10))
+
     expect(yasnyfy('тест')).toEqual('\n>2k17\n>тест\nЯсно')
     expect(yasnyfy('')).toEqual('\n>2k17\nЯсно')
 
-    Settings.now = (): number => new Date(2019, 10, 10).valueOf()
+    jest.setSystemTime(new Date(2019, 10, 10))
     expect(yasnyfy('тест')).toEqual('\n>2k19\n>тест\nЯсно')
     expect(yasnyfy('')).toEqual('\n>2k19\nЯсно')
 
-    Settings.now = (): number => new Date(2021, 10, 10).valueOf()
+    jest.setSystemTime(new Date(2021, 10, 10))
     expect(yasnyfy('тест')).toEqual('\n>2️⃣0️⃣2️⃣1️⃣\n>тест\nЯсно')
     expect(yasnyfy('')).toEqual('\n>2️⃣0️⃣2️⃣1️⃣\nЯсно')
   })
 
   test('yasnyfy should properly work with warhammer values', () => {
-    Settings.now = (): number => new Date(2020, 10, 10).valueOf()
+    jest.setSystemTime(new Date(2020, 10, 10))
     expect(yasnyfy('тест').slice(7)).toEqual(' 020.M3\n>тест\nЯсно')
     expect(yasnyfy('').slice(7)).toEqual(' 020.M3\nЯсно')
   })
 
   test('yasnyfy should fallback to default value', () => {
-    Settings.now = (): number => new Date(2016, 10, 10).valueOf()
+    jest.setSystemTime(new Date(2016, 10, 10))
     expect(yasnyfy('тест')).toEqual('\n>2016\n>тест\nЯсно')
     expect(yasnyfy('')).toEqual('\n>2016\nЯсно')
   })
 
   test('yasnyfy should properly work at 1st of april', () => {
-    Settings.now = (): number => new Date(2019, 3, 1, 16, 0).valueOf()
+    jest.setSystemTime(new Date(2019, 3, 1, 16, 0))
 
     expect(yasnyfy('тест')).toEqual('\n>1 Апреля 19 года\n>тест\nЯсно😐')
     expect(yasnyfy('')).toEqual('\n>1 Апреля 19 года\nЯсно😐')
--- src/text/yasno.ts
+++ src/text/yasno.ts
@@ -1,13 +1,15 @@
 import { padStart } from 'lodash'
-import { DateTime } from 'luxon'
 
 const whDate = (): string => {
   // https://warhammer40k.fandom.com/wiki/Imperial_Dating_System
   const secondsInYear = 31556926
   const secondsInFraction = 31557
-  const currentTimeInSeconds = Math.floor(
-    DateTime.local().setZone('Europe/Minsk').toMillis() / 1000,
+  const date = new Date(
+    new Date().toLocaleString('en-US', {
+      timeZone: 'Europe/Minsk',
+    }),
   )
+  const currentTimeInSeconds = date.valueOf() / 1000
   const yearsFromEpochStart = Math.floor(currentTimeInSeconds / secondsInYear)
   const currentYearStart = yearsFromEpochStart * secondsInYear
   const yearFraction = Math.floor((currentTimeInSeconds - currentYearStart) / secondsInFraction)
@@ -28,12 +30,18 @@ const YEARS = {
 }
 
 export const yasnyfy = (text: string): string => {
-  const { month, day, year } = DateTime.local().setZone('Europe/Minsk').toObject()
+  const date = new Date(
+    new Date().toLocaleString('en-US', {
+      timeZone: 'Europe/Minsk',
+    }),
+  )
+  const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()]
+
   const stringYear = String(year)
   const formattedYear = YEARS[stringYear]?.() ?? stringYear
   const quotedText = text ? `\n>${text}` : ''
 
-  if (month === 4 && day === 1) {
+  if (month === 3 && day === 1) {
     return `\n>1 Апреля ${stringYear.slice(2)} года${quotedText}\nЯсно😐`
   }
   return `\n>${formattedYear}${quotedText}\nЯсно`
