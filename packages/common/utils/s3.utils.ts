d52f80d608d4fd686e8de8ce16cd31ea26218013
alisamonstr <47886612+alisamonstr@users.noreply.github.com>
2022-04-21 12:00:55 +0300

    set crypto currencies price request once in 5 minutes (#15)
    * set crypto currencies price request once in 5 minutes
    
    * rename file, fix prettier format

---
+++ packages/common/utils/s3.utils.ts
@@ -0,0 +1,16 @@
+import { S3 } from 'aws-sdk'
+
+const s3 = new S3({
+  region: process.env.region,
+  apiVersion: '2006-03-01',
+})
+
+export const getFile = async (Bucket: S3.Bucket, Key: string): Promise<S3.Types.GetObjectOutput> =>
+  s3.getObject({ Bucket, Key } as S3.GetObjectRequest).promise()
+
+export const saveFile = async (
+  Bucket: S3.Bucket,
+  Key: string,
+  Body: Buffer,
+): Promise<S3.Types.PutObjectOutput> =>
+  s3.putObject({ Bucket, Key, Body } as S3.PutObjectRequest).promise()
--- packages/lambdas/telegram-bot/bot/currency/crypto-currency.ts
+++ packages/lambdas/telegram-bot/bot/currency/crypto-currency.ts
@@ -1,8 +1,12 @@
 import axios from 'axios'
 import { round } from 'lodash'
+import { getFile, getRoundedDate, saveFile } from '@tg-bot/common'
+import { Bucket } from 'aws-sdk/clients/s3'
 
 const timeout = 15000
 const coinMarketCapApiKey = process.env.COIN_MARKET_CAP_API_KEY || 'set_your_token'
+const cryptoRequestsBucketName = process.env.CRYPTO_REQUESTS_BUCKET_NAME as Bucket
+const symbols = { BTC: 2, ETH: 2, ADA: 3, CERE: 4 }
 
 const getPoloniexData = async (): Promise<string> => {
   const url = 'https://poloniex.com/public?command=returnTicker'
@@ -28,23 +32,12 @@ interface CoinMarketCurrency {
     USD: {
       price: number
       percent_change_1h: number
+      percent_change_24h: number
     }
   }
 }
 
-const getCoinMarketCapData = async (): Promise<string> => {
-  const symbols = { BTC: 2, ETH: 2, ADA: 3, CERE: 4 }
-  const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-
-  const response = await axios(url, {
-    timeout,
-    params: { start: 1, limit: 5_000, convert: 'USD' },
-    headers: {
-      'X-CMC_PRO_API_KEY': coinMarketCapApiKey,
-    },
-  })
-
-  const currencies = response.data.data
+const getCurrenciesResult = (currencies: CoinMarketCurrency[]) => {
   const data = Object.keys(symbols).map((symbol) => {
     const currency = currencies.find((c: CoinMarketCurrency) => c.symbol === symbol)
 
@@ -59,8 +52,35 @@ const getCoinMarketCapData = async (): Promise<string> => {
 
     return `${symbol}: ${formattedPrice} (${isUp ? '+' : ''}${priceChange}%)`
   })
-
   return `Курсы криптовалют:\n${data.join('\n')}`
+}
+
+const getCoinMarketCapData = async (): Promise<string> => {
+  const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+  const roundedDate = String(getRoundedDate(5))
+  const savedDataBuffer = await getFile(cryptoRequestsBucketName, roundedDate).catch(() => null)
+  const savedDate = JSON.parse(savedDataBuffer?.Body?.toString() || '{}')
+
+  if (!savedDataBuffer) {
+    const response = await axios(url, {
+      timeout,
+      params: { start: 1, limit: 5_000, convert: 'USD' },
+      headers: {
+        'X-CMC_PRO_API_KEY': coinMarketCapApiKey,
+      },
+    })
+    const currencies = response.data.data
+    const filteredCurrencies = currencies.filter((c: CoinMarketCurrency) => symbols[c.symbol])
+    await saveFile(
+      cryptoRequestsBucketName,
+      roundedDate,
+      Buffer.from(JSON.stringify(filteredCurrencies)),
+    )
+
+    return getCurrenciesResult(filteredCurrencies)
+  }
+
+  return getCurrenciesResult(savedDate)
 }
 
 export const getCryptoCurrency = async (): Promise<string> => {
