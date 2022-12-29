083880386feb53d9a9d5317cea6c9036bdb51093
Eugene Draitsev <ddrrai@gmail.com>
2022-12-29 11:34:39 +0100

    Replace FCC with exchangerate host. Dependencies update

--- packages/lambdas/telegram-bot/src/currency/main-currency.ts
+++ packages/lambdas/telegram-bot/src/currency/main-currency.ts
@@ -1,34 +1,10 @@
 import axios from 'axios'
-import { map, round, chunk } from 'lodash'
+import { round } from 'lodash'
 
-const fccApiKey = process.env.FCC_API_KEY || 'set_your_token'
 const fixerKey = process.env.FIXER_API_KEY || 'set_your_token'
 const timeout = 15000
 
-const getFreeCurrencyData = async (): Promise<string> => {
-  const currencies = ['USD_BYN', 'EUR_BYN', 'USD_SEK', 'EUR_SEK', 'USD_PLN', 'EUR_PLN', 'USD_TRY']
-  const url = 'https://free.currconv.com/api/v7/convert'
-
-  const promises = chunk(currencies, 2).map((pair) =>
-    axios(url, {
-      timeout,
-      params: { compact: 'y', apiKey: fccApiKey, q: pair.join(',') },
-    }).then((x) => x.data),
-  )
-
-  const result = await Promise.all(promises)
-  const mergedResult = Object.assign({}, ...result)
-
-  const currencyMessage = map(
-    mergedResult,
-    (value, key) => `${key.replace('_', '/')}: ${round(value.val, 2)}`,
-  ).join('\n')
-
-  return `Курсы FCC:\n${currencyMessage}\n`
-}
-
-const getFixerData = async (): Promise<string> => {
-  const url = 'http://data.fixer.io/api/latest'
+const getExchangeRateData = async (url: string, provider: string): Promise<string> => {
   const { rates } = await axios(url, {
     timeout,
     params: { access_key: fixerKey, format: 1, base: 'EUR' },
@@ -39,7 +15,7 @@ const getFixerData = async (): Promise<string> => {
     return x.data
   })
 
-  return `Курсы fixer:\
+  return `Курсы ${provider}:\
           \nUSD/BYN: ${round(rates.BYN / rates.USD, 3)}\
           \nEUR/BYN: ${round(rates.BYN, 3)}\
           \nUSD/SEK: ${round(rates.SEK / rates.USD, 3)}\
@@ -52,10 +28,15 @@ const getFixerData = async (): Promise<string> => {
 
 export const getMainCurrencies = async (): Promise<string> => {
   try {
-    return await getFreeCurrencyData()
+    const url = 'https://api.exchangerate.host/latest'
+    const provider = 'exchangerate host'
+    return await getExchangeRateData(url, provider)
   } catch (e) {
-    console.error('FCC API error', e)
-    return getFixerData().catch((err) => {
+    console.error('exchangerate host error', e)
+    const url = 'http://data.fixer.io/api/latest'
+    const provider = 'fixer'
+
+    return getExchangeRateData(url, provider).catch((err) => {
       console.error('Fixer API error', err)
       throw err
     })
--- packages/lambdas/telegram-bot/src/text/__tests__/yasno.test.ts
+++ packages/lambdas/telegram-bot/src/text/__tests__/yasno.test.ts
@@ -27,6 +27,14 @@ describe('yasnyfy should works as designed', () => {
     jest.setSystemTime(new Date(2022, 10, 10))
     const result = yasnyfy('тест')
     expect(result.includes('² ⁰ ² ²') || result.includes('２０２２')).toBeTruthy()
+
+    jest.setSystemTime(new Date(2023, 10, 10))
+    const result2023 = yasnyfy('тест')
+    expect(
+      result2023.includes('202 :3') ||
+        result2023.includes('2023') ||
+        result2023.includes('MMXXIII'),
+    ).toBeTruthy()
   })
 
   test('yasnyfy should properly work with warhammer values', () => {
