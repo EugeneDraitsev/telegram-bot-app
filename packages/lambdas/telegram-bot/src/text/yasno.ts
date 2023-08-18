546df466e2d843eae2eacfcb90c8c2c12a174c0e
EugeneDraitsev <eugene.draitsev@yubico.com>
2023-08-18 11:50:20 +0200

    Fix yasno whDate for 2023

--- packages/lambdas/telegram-bot/src/text/yasno.ts
+++ packages/lambdas/telegram-bot/src/text/yasno.ts
@@ -7,14 +7,15 @@ const whDate = (): string => {
       timeZone: 'Europe/Minsk',
     }),
   )
-  const startOfYear = new Date(date.getFullYear(), 1, 1, 0, 0, 0 ).valueOf() 
-  const endOfYear = new Date(date.getFullYear(), 11, 31, 23, 59, 59 ).valueOf() 
+  const startOfYear = new Date(date.getFullYear(), 1, 1, 0, 0, 0).valueOf()
+  const endOfYear = new Date(date.getFullYear(), 11, 31, 23, 59, 59).valueOf()
   const secondsInCurrentYear = Math.ceil((endOfYear - startOfYear) / 1000)
   const secondsInFraction = Math.ceil(secondsInCurrentYear / 1000)
-  const currentTime = date.valueOf() 
+  const currentTime = date.valueOf()
   const yearFraction = Math.floor(
-  (currentTime - startOfYear ) / 1000 / secondsInFraction)
-  const currentYear = date.getFullYear() 
+    (currentTime - startOfYear) / 1000 / secondsInFraction,
+  )
+  const currentYear = date.getFullYear()
   const millenium = Math.floor(currentYear / 1000) + 1
   return `0 ${padStart(String(yearFraction), 3, '0')} ${String(
     currentYear,
@@ -28,7 +29,7 @@ const YEARS = {
   2020: whDate,
   2021: () => '2️⃣0️⃣2️⃣1️⃣',
   2022: () => sample(['² ⁰ ² ²', '２０２２']),
-  2023: () => `||${sample(['202 :3', '2023', 'MMXXIII', whDate])}||`,
+  2023: () => `||${sample(['202 :3', '2023', 'MMXXIII', whDate()])}||`,
 }
 
 type Year = keyof typeof YEARS
@@ -43,15 +44,15 @@ export const yasnyfy = (text: string): string => {
     date.getMonth(),
     date.getDate(),
     date.getFullYear(),
-    date.getDay()
+    date.getDay(),
   ]
-  
+
   const formattedYear = YEARS[year as Year]?.() ?? year
   const quotedText = text ? `\n\\>${text}` : ''
 
   if (month === 3 && day === 1) {
     return `\n\\>1 Апреля ${String(year).slice(2)} года${quotedText}\nЯсно😐`
-  } else if (weekDay === 4){
+  } else if (weekDay === 4) {
     return `\n\\>${formattedYear}${quotedText}\nЯсна`
   }
   return `\n\\>${formattedYear}${quotedText}\nЯсно`
