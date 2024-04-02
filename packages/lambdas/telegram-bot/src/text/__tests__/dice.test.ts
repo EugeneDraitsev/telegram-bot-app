f84217e1381b59a1ae9b91f4a82b8ea6e2d36033
Eugene Draitsev <ddrrai@gmail.com>
2024-04-02 18:59:13 +0200

    Replace yarn with bun (#22)
    * Replace yarn with bun
    
    * Fix tests for bun
    
    * Remove cache part of the workflow

--- packages/lambdas/telegram-bot/src/text/__tests__/dice.test.ts
+++ packages/lambdas/telegram-bot/src/text/__tests__/dice.test.ts
@@ -1,12 +1,8 @@
-import lodash from 'lodash'
-
 import { throwDice } from '../dice'
-
-lodash.random = jest.fn(() => 1)
 
 describe('throwDice should works as designed', () => {
   test('throwDice should return correct dices', () => {
-    expect(throwDice(6)).toEqual(`<pre>
+    expect(throwDice(6, () => 1)).toEqual(`<pre>
   ___
 /     \\
 | 001 |
