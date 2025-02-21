cb1cd0d8b0c53fa2fd3acac0928f1012a40ae93f
EugeneDraitsev <ddrrai@gmail.com>
2025-02-21 13:14:46 +0100

    Refine chat history instructions for clarity and consistency

--- src/telegram-bot/src/utils/ai.utils.ts
+++ src/telegram-bot/src/utils/ai.utils.ts
@@ -19,5 +19,6 @@ export const geminiSystemInstructions = `
   
   You also will be provided with chat history for last 24 hours (if available) in JSON format from telegram.
   It could contain a previous command to you (if message started with /, like /g, /q, /qq, /z etc.) and your previous responses
-  Try to rely mostly on more recent posts, but don't ignore older posts. You *don't need* to include or quote history in your answers, just try to stay in context.
+  Try to rely mostly on more recent posts, but don't ignore older posts. 
+  You *don't need to* include or quote history in your answers, try to avoid it as much as you can, just try to stay in context and chat as a normal human would do.
 `
