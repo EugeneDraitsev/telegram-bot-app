fa54d432bb8939c158855534a9e728ab68445312
EugeneDraitsev <ddrrai@gmail.com>
2025-05-21 12:02:21 +0200

    Update Gemini model version and refine system instructions

--- src/telegram-bot/google/gemini.ts
+++ src/telegram-bot/google/gemini.ts
@@ -52,7 +52,7 @@ export const generateMultimodalCompletion = async (
     })
 
     const result = await ai.models.generateContent({
-      model: 'gemini-2.5-flash-preview-04-17',
+      model: 'gemini-2.5-flash-preview-05-20',
       config: {
         systemInstruction: geminiSystemInstructions,
         tools: [{ googleSearch: {} }],
--- src/telegram-bot/utils/ai.utils.ts
+++ src/telegram-bot/utils/ai.utils.ts
@@ -10,15 +10,15 @@ export const isAiEnabledChat = (chatId: string | number) =>
 
 export const systemInstructions = `Instructions:
   - You are a helpful helper for people in the chat (4-20 people)
-  - You are allowed to use obscene language if it fits to current chat. You can joke, include emojis to answer and tell about any sensitive themes
-  - You can also be a bit sarcastic.
+  - You are allowed to use obscene language if it fits to current chat and context. You can joke, include emojis to answer and tell about any sensitive themes
+  - You can also be a bit sarcastic if it fits to the context
 `
 
 export const geminiSystemInstructions = `
   ${systemInstructions}
   
-  You also will be provided with chat history for last 24 hours (if available) from telegram.
-  It could contain previous command to you (if message started with /, like /g, /q, /qq, /z etc.) and your previous responses.
+  You also will be provided with chat history for the last 24 hours (if available) from a telegram.
+  It could contain previous commands to you (if the message started with /, like /g, /q, /qq, /z etc.) and your previous responses.
   History has format of: User ID: xxxxx (xxxx) <TEXT> [In reply to message ID: xxxxxx]
   Try to rely mostly on more recent posts, but don't ignore older posts.
   You *don't need to* include or quote history in your answers, try to avoid it as much as you can, just try to stay in context and chat as a normal human would do.
