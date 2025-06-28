7bd2a78c0c451b7386a0cb895b42d2fe8419ffd8
EugeneDraitsev <ddrrai@gmail.com>
2025-06-28 22:37:18 +0200

    Update dependencies and change gemini multimodal model

--- src/telegram-bot/google/gemini.ts
+++ src/telegram-bot/google/gemini.ts
@@ -52,7 +52,7 @@ export const generateMultimodalCompletion = async (
     })
 
     const result = await ai.models.generateContent({
-      model: 'gemini-2.5-flash-preview-05-20',
+      model: 'gemini-2.5-flash',
       config: {
         systemInstruction: geminiSystemInstructions,
         tools: [{ googleSearch: {} }],
